7239602eb33540decd7e63e2e488536e7ec1682c
Mahlet Seifu <110999391+Mahlet2123@users.noreply.github.com>
2023-04-19 22:55:57 +0300

    update game.js

--- game.js
+++ game.js
@@ -1,1 +1,10 @@
+// returns either "Rock", "Paper", or "Scissors" randomly
+function getComputerChoice() {
+  const choices = ['Rock', 'Paper', 'Scissors'];
+  const randomNumber = Math.floor(Math.random() * 3);
+  return choices[randomNumber];
+}
 
+// returns a string declaring the winner of the round
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
